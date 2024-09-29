86c1118f4174381328b1a8c17ebf2ae64a4734fa
Itxadii161 <itxadii161@gmail.com>
2024-09-29 00:25:20 -0700

    js for calculate button is created

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-var yearArea = document.getElementById("yearArea");
-var monthArea = document.getElementById("monthArea");
-var dayArea = document.getElementById("dayArea");
+
+
+function findDate() {
+    var yearArea = document.getElementById("yearArea");
+    var monthArea = document.getElementById("monthArea");
+    var dayArea = document.getElementById("dayArea");
+
+    const selectDate = document.getElementById('selectDate').value;
+
+    if (selectDate) {
+        const year = new Date(selectDate).getFullYear();
+        yearArea.textContent = `${year} Year`;
+
+        const month = new Date(selectDate).getMonth();
+        monthArea.textContent = `${month} Month`;
+
+        const day = new Date(selectDate).getDate();
+        dayArea.textContent = `${day} Day`;
+    } else {
+        yearArea.textContent = '0';
+        monthArea.textContent = '0.';
+        dayArea.textContent = '0';
+    }
+}
+ 
